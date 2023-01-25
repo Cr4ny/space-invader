d5e41e94ddcd43937966c7a396bd5ce92a604f48
Sebastian Bodo Hennicke <sebastian.hennicke@brightstraining.com>
2023-01-25 15:08:05 +0100

    nur noch Bewegung auf x oder y achse möglich, nicht schräg

--- src/main/java/com/brightstraining/javafxgame/InputHandler.java
+++ src/main/java/com/brightstraining/javafxgame/InputHandler.java
@@ -13,14 +13,22 @@ public class InputHandler {
 
     public void onKeyPressed(KeyCode keyCode) {
         if (keyCode == KeyCode.LEFT) {
+            model.getPlayer().stopMovingDown();
+            model.getPlayer().stopMovingUp();
             model.getPlayer().startMovingLeft();
         }
         else if (keyCode == KeyCode.RIGHT) {
+            model.getPlayer().stopMovingDown();
+            model.getPlayer().stopMovingUp();
             model.getPlayer().startMovingRight();
         }
         else if(keyCode == KeyCode.UP) {
+            model.getPlayer().stopMovingLeft();
+            model.getPlayer().stopMovingRight();
             model.getPlayer().startMovingUp();
         }else if(keyCode == KeyCode.DOWN) {
+            model.getPlayer().stopMovingLeft();
+            model.getPlayer().stopMovingRight();
             model.getPlayer().startMovingDown();
         }
     }
